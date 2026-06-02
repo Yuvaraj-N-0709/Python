128208e86f689c2ca09a8b0bb4f8de7c4de5aff3
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-06-02 13:21:03 +0530

    Add files via upload

---
+++ NoReturnTypeWithoutArgumentFunction.py
@@ -0,0 +1,8 @@
+#No Return Type Without Argument Function in Python
+def add():
+    a=int(input("Enter The Value Of A : "))
+    b=int(input("Enter The Value Of B : "))
+    c=a+b
+    print("Total ",c)
+    
+add()
