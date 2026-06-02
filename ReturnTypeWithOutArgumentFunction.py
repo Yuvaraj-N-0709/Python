e8cb58eb81e5bc1531bdacf9a74bdada03e10d88
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-06-02 13:21:52 +0530

    Add files via upload

---
+++ ReturnTypeWithOutArgumentFunction.py
@@ -0,0 +1,10 @@
+#Return Type Without Argument Function in Python
+
+def mul():
+    a=int(input("Enter The Value Of A : "))
+    b=int(input("Enter The Value Of B : "))
+    c=a*b
+    return c
+    
+x=mul()
+print("mul",x)
