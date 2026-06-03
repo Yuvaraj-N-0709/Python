565065b1d82519ad7495e70e42dc5837a5ada7ed
Yuvaraj N <yuvarajnarayanasamy28@gmail.com>
2026-06-03 12:25:54 +0530

    Add files via upload

---
+++ MathFunction.py
@@ -0,0 +1,15 @@
+#math function
+
+import math as m
+
+print(m.sqrt(4))
+print(m.ceil(1.55555))
+print(m.floor(1.55555))
+print(m.factorial(5))
+print(m.fabs(-5))
+print(m.pow(2,3))
+print(m.log2(2))
+print(m.log10(2))
+print(m.pi)
+print(m.e)
+
